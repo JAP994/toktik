3ae1d6743d3d402e39f8771a245ece2e46d582fb
JAP994 <sucop@MacBook-Pro-de-Jonathan.local>
2025-08-19 11:19:37 -0500

    toktik final app v.0

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/widgets/shared/video_schollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+
     return Scaffold(
-      body: Center(
-        child: Text('DiscoverScreen'),
-      ),
+      body: discoverProvider.initianlLoading
+        ? const Center( child: CircularProgressIndicator( strokeWidth: 2,))
+        : VideoSchollableView(videos: discoverProvider.videos)
     );
   }
 }
